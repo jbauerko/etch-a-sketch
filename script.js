320fae4d6fcf4af93d0e9c6a55e3a6e2c276b8c4
Julian Bauer-Kong <jbauerkong@gmail.com>
2025-03-20 22:01:59 -0400

    Add generate grid button

--- script.js
+++ script.js
@@ -1,22 +1,47 @@
 const grid = document.querySelector(".container");
-for (let i = 0; i < 16; i++) {
-    let row = document.createElement("div");
-    row.classList.add("row")
-    grid.appendChild(row);
 
-    for (let j = 0; j < 16; j++) {
-        let tile = document.createElement("div");
-        tile.classList.add("tile")
-        row.appendChild(tile);
-    }   
+grid.addEventListener("delete", () => {
+    while (grid.firstChild) {
+        grid.removeChild(grid.firstChild);
+    }
+});
+
+function generateGrid(size) {
+    for (let i = 0; i < size; i++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        grid.appendChild(row);
+    
+        for (let j = 0; j < size; j++) {
+            let tile = document.createElement("div");
+            tile.classList.add("tile");
+            tile.addEventListener("mouseover", () => {
+                tile.style.backgroundColor = "black";
+            });
+            tile.addEventListener("mouseout", () => {
+                tile.style.backgroundColor = "white";
+            });
+            row.appendChild(tile);
+        }
+    }
 }
 
-const tiles = document.querySelectorAll(".tile");
-tiles.forEach(tile => {
-    tile.addEventListener("mouseover", () => {
-        tile.style.backgroundColor = "black";
-    });
-    tile.addEventListener("mouseout", () => {
-        tile.style.backgroundColor = "white";
-    });
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+    let grid_size = prompt("Enter grid size: ");
+    if (grid_size > 100) {
+        alert("That's too large!");
+    } else {
+        let delete_event = new CustomEvent("delete");
+        grid.dispatchEvent(delete_event);
+        generateGrid(grid_size);  
+    }
 });
+
+
+
+
+
+
+
+
